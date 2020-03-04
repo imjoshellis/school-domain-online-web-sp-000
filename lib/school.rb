88792bd3c2325640f6b76fb720bf71f99e877515
Josh Ellis <joshe523@gmail.com>
2020-03-04 04:18:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
-# code here!
+class School
+  attr_accessor :roster
+
+  def initialized(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] = [] unless @roster[grade].exists?
+    @roster[grade] << name
+  end
+end
